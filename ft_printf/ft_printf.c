a40c7530b82549d374bbdb1f3bbc4000f7f94e16
mehrasmeydani <52368993+mehrasmeydani@users.noreply.github.com>
2025-04-23 14:29:49 +0000

    lol

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: megardes <megardes@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 11:38:04 by megardes          #+#    #+#             */
-/*   Updated: 2025/04/23 12:21:42 by megardes         ###   ########.fr       */
+/*   Updated: 2025/04/23 14:28:38 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,38 @@ int	ft_putstr_pf(char *str)
 	return ((int)write(1, str, ft_strlen(str)));
 }
 
+void	ft_putp(unsigned long long a, int *out, int run)
+{
+	if (a == 0 && run == 0)
+	{
+		*out += write(1, "(nil)", 5);
+		return;
+	}
+	else if (run == 0)
+		*out += write(1, "0x", 2);
+	if (a >= 16)
+	{
+		ft_putp(a / 16, out, run + 1);
+		ft_putp(a % 16, out, run + 1);
+	}
+	else
+		*out += write (1, &"0123456789abcdef"[a], 1);
+}
+
 void	ft_putd(long long a, int c, int *out, int run)
 {
-	const char	*b[3] = {"0123456789", "0123456789abcdef", "0123456789abcdef"};
+	const char	*b[3] = {"0123456789", "0123456789abcdef", "0123456789ABCDEF"};
 
-	if (a == 0 && c == 2)
-		*out += write(1, "(nil)", 1);
-	else if (c == 2 && run == 0)
-		*out += write(1, "0x", 2);
 	if (a < 0)
 	{
 		a = -a;
-		*out += write(1, "-", 2);
+		*out += write(1, "-", 1);
 		ft_putd(a, c, out, run + 1);
 	}
-	else if (a > (long long)ft_strlen(b[c]))
+	else if (a >= (long long)ft_strlen(b[c]))
 	{
 		ft_putd(a / ft_strlen(b[c]), c, out, run + 1);
-		ft_putd(a % ft_strlen(b[c]), c, out, run);
+		ft_putd(a % ft_strlen(b[c]), c, out, run + 1);
 	}
 	else
 		*out += write (1, &b[c][a], 1);
@@ -52,16 +66,16 @@ int	print(const char *str, int *i, va_list args)
 	else if (str[(*i)] == '%' && str[(*i) + 1] == 's' && ++(*i))
 		out += ft_putstr_pf(va_arg(args, char *));
 	else if (str[(*i)] == '%' && str[(*i) + 1] == 'p' && ++(*i))
-		ft_putd(va_arg(args, unsigned long long), 2, &out, 0);
+		ft_putp(va_arg(args, unsigned long long), &out, 0);
 	else if (str[(*i)] == '%' && (str[(*i) + 1] == 'd'
-			|| str[(*i) + 1] != 'd') && ++(*i))
+			|| str[(*i) + 1] == 'i') && ++(*i))
 		ft_putd(va_arg(args, int), 0, &out, 0);
 	else if (str[(*i)] == '%' && str[(*i) + 1] == 'u' && ++(*i))
 		ft_putd(va_arg(args, unsigned int), 0, &out, 0);
 	else if (str[(*i)] == '%' && str[(*i) + 1] == 'x' && ++(*i))
-		ft_putd(va_arg(args, int), 1, &out, 0);
+		ft_putd((unsigned int)va_arg(args, unsigned int), 1, &out, 0);
 	else if (str[(*i)] == '%' && str[(*i) + 1] == 'X' && ++(*i))
-		ft_putd(va_arg(args, unsigned int), 1, &out, 0);
+		ft_putd(va_arg(args, unsigned int), 2, &out, 0);
 	else if (str[(*i)] == '%' && str[(*i) + 1] == '%' && ++(*i))
 		out += ft_putchar_fd(str[(*i)], 1);
 	else
--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,7 +1,8 @@
 #include "ft_printf.h"
 #include <stdio.h>
+#include <limits.h>
 int main()
 {
-	char	*str = (char *)1;
-	ft_printf("%d", ft_printf("%p", str));
+	char	*str = (char *)-1;
+	ft_printf("%p", LONG_MIN);
 }
