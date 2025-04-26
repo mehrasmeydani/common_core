65747730eea7617f3df1091be5417cd1c159bdd4
Mehras Gardesh Meydani <megardes@c3r10p8.42vienna.com>
2025-04-26 17:36:44 +0200

    done

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,15 +6,67 @@
 /*   By: megardes <megardes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 13:31:02 by megardes          #+#    #+#             */
-/*   Updated: 2025/04/26 15:29:20 by megardes         ###   ########.fr       */
+/*   Updated: 2025/04/26 17:33:51 by megardes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*out;
+	char	*tmp;
+	size_t	i;
+
+	i = 0;
+	if (size == 0 || nmemb == 0)
+		return (malloc(0));
+	if (nmemb == SIZE_MAX || size == SIZE_MAX
+		|| (size * nmemb) / nmemb != size || size * nmemb > INT32_MAX)
+		return (NULL);
+	out = (void *)malloc(nmemb * size);
+	if (!out)
+		return (NULL);
+	tmp = (char *)out;
+	while (i < size)
+	{
+		tmp[i] = 0;
+		i++;
+	}
+	return (out);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dsize)
+{
+	size_t	dst_len;
+	size_t	i;
+
+	if ((dsize == 0 && !dst) || dsize <= ft_strlen(dst))
+		return (ft_strlen(src) + dsize);
+	dst_len = ft_strlen(dst);
+	i = 0;
+	while (src[i] != '\0' && ((i + dst_len) < dsize - 1))
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (ft_strlen(src) + dst_len);
+}
+
 char	*ft_strchr(const char *s, int c)
 {
-	int		i;
+	ssize_t	i;
 	char	*tmp;
 
 	i = -1;
@@ -28,24 +80,66 @@ char	*ft_strchr(const char *s, int c)
 
 char	*ft_relocat(char *in, char buff[BUFFER_SIZE])
 {
-	char *out;
+	char	*out;
+	size_t	mlen;
+
+	mlen = ft_strlen(buff) + 1;
+	if (!in)
+	{
+		out = (char *)ft_calloc(mlen, 1);
+		if (!out)
+			return (NULL);
+		ft_strlcat(out, buff, mlen);
+		return (out);
+	}
+	out = (char *)ft_calloc(ft_strlen(in) + mlen, 1);
+	if (!out)
+		return (NULL);
+	ft_strlcat(out, in, ft_strlen(in) + 1);
+	ft_strlcat(out, buff, ft_strlen(out) + mlen);
+	free(in);
+	return (out);
+}
+
+char	*left(char *in)
+{
+	char	*out;
+	char	*tmp;
+
+	tmp = ft_strchr(in, '\n') + 1;
+	out = (char *)ft_calloc(ft_strlen(tmp) + 1, 1);
+	if (!out)
+		return (free(in), NULL);
+	ft_strlcat(out, tmp, ft_strlen(tmp) + 1);
+	*(ft_strchr(in, '\n') + 1) = 0;
+	return (out);
 }
 
 char	*get_next_line(int fd)
 {
 	char		buff[BUFFER_SIZE];
-	static char *red = NULL;
+	static char	*red = NULL;
+	char		*out;
 	ssize_t		r;
 
+	out = red;
 	if (fd < 0)
 		return (NULL);
-	while (!red || !ft_strchr(red, '\n'))
+	while (!out || !ft_strchr(out, '\n'))
 	{
 		r = read(fd, buff, BUFFER_SIZE);
 		if (r == -1)
-			return (free(red), red = NULL, NULL);
+			return (free(out), out = NULL, free(red), red = NULL, NULL);
 		if (r == 0)
-			break;
-		red = ft_relocat(red, buff);
+			break ;
+		out = ft_relocat(out, buff);
+		if (!out)
+			return (free(out), out = NULL, free(red), red = NULL, NULL);
 	}
+	if (ft_strchr(out, '\n'))
+	{
+		red = left(out);
+		return (out);
+	}
+	return (red = NULL, out);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,17 +6,18 @@
 /*   By: megardes <megardes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 13:29:54 by megardes          #+#    #+#             */
-/*   Updated: 2025/04/26 13:42:25 by megardes         ###   ########.fr       */
+/*   Updated: 2025/04/26 17:19:30 by megardes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define geT_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # define BUFFER_SIZE 42
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdint.h>
 
 char	*get_next_line(int fd);
 
