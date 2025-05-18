184f1d335314ed7163a1a101284e4fdb365a99e9
mehrasmeydani <52368993+mehrasmeydani@users.noreply.github.com>
2025-05-18 23:14:50 +0000

    lol

--- pipex/test.c
+++ pipex/test.c
@@ -6,16 +6,19 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 15:56:42 by megardes          #+#    #+#             */
-/*   Updated: 2025/05/18 18:11:51 by codespace        ###   ########.fr       */
+/*   Updated: 2025/05/18 23:13:10 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header/pipex.h"
+#include <stdio.h>
 
 int main(void)
 {
 	pid_t	pid;
+	pid_t	pid2;
 	int		status;
+	//int		status2;
 	char	*env[] = {NULL};
 	char	*av[] = {"/usr/bin/ls", "-l", "-a", NULL};	
 	char	*av2[] = {"/bin/grep", "srcs", NULL};
@@ -26,38 +29,39 @@ int main(void)
 	pid	= fork();
 	if (pid == 0)
 	{
+		if (dup2(fd[1], 1) == -1)
+			return (ft_putstr_fd("what", 1), 1);
+		close(fd[0]);
+		close(fd[1]);
+		execve(av[0], av, env);
+	}
+	else
+	{
+		waitpid(pid, &status, WNOHANG);
 		int		fd1[2] = {};
 		pipe(fd1);
-		pid = fork();
-		if (pid == 0)
+		pid2 = fork();
+		status = 0;
+		if (pid2 == 0)
 		{
-			if (dup2(fd1[1], 1) == -1)
-				return (ft_putstr_fd("what", 1), 1);
-			close(fd1[0]);
-			close(fd1[1]);
-			close(fd[0]);
-			close(fd[1]);
-			execve(av[0], av, env);
-		}
-		else
-		{
-			waitpid(pid, &status, 0);
-			dup2(fd1[0], 0);
-			dup2(fd[1], 1);
+			dup2(fd[0], 0);
+			dup2(fd1[1], 1);
 			close(fd[0]);
 			close(fd[1]);
 			close(fd1[0]);
 			close(fd1[1]);
 			execve(av2[0], av2, env);
 		}
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-		dup2(fd[0], 0);
-		close(fd[0]);
-		close(fd[1]);
-		execve(av3[0], av3, env);
+		else
+		{
+			waitpid(pid2, &status, WNOHANG);
+			dup2(fd1[0], 0);
+			close(fd[0]);
+			close(fd[1]);
+			close(fd1[0]);
+			close(fd1[1]);
+			return(execve(av3[0], av3, env));
+		}
 	}
 	write(1, "what", 4);
 }
