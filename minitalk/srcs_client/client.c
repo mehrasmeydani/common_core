999d7459bc46fdc6af8ad9d9656ee125a0d1ce12
Mehras Gardesh Meydani <megardes@c3r9p7.42vienna.com>
2025-05-30 11:26:45 +0200

    done

--- minitalk/srcs_client/client.c
+++ minitalk/srcs_client/client.c
@@ -6,13 +6,68 @@
 /*   By: megardes <megardes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 13:05:26 by megardes          #+#    #+#             */
-/*   Updated: 2025/05/26 13:11:45 by megardes         ###   ########.fr       */
+/*   Updated: 2025/05/30 11:19:08 by megardes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minitalk.h"
 
+int	g_pending = 0;
+
+void	get_respond(int signum, siginfo_t *new, void *old)
+{
+	g_pending = 0;
+	(void)old;
+	(void)new;
+	if (signum == SIGUSR1)
+		return (ft_putendl_fd("Server recieved message succesfully", 1));
+}
+
+void	send_message(char *in, pid_t pid)
+{
+	ssize_t	i;
+	ssize_t	bit;
+	ssize_t	len;
+	t_uc	c;
+
+	len = ft_strlen(in);
+	i = -1;
+	while (++i <= len)
+	{
+		c = in[i];
+		bit = -1;
+		while (++bit < 8)
+		{
+			g_pending = 1;
+			if (c & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			while (g_pending != 0 && g_pending++ < 3)
+				usleep(0);
+			if (g_pending == 3)
+				return (ft_putendl_fd("No respond from server in time", 2));
+			c = c >> 1;
+		}
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	
+	ssize_t				i;
+	struct sigaction	sig;
+
+	i = -1;
+	if (argc != 3)
+		return (ft_putendl_fd("Error: ./client <server_pid> <message>", 2), 1);
+	while (argv[1][++i])
+		if (!ft_isdigit(argv[1][i]))
+			return (ft_putendl_fd("Error: invalid pid", 2), 1);
+	sig.sa_sigaction = &get_respond;
+	sig.sa_flags = SA_SIGINFO | SA_RESTART;
+	if (sigemptyset(&(sig.sa_mask)))
+		return (ft_putendl_fd("sigemptyset failed", 2), 1);
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
+	send_message(argv[2], ft_atoi(argv[1]));
 }
--- minitalk/srcs_server/server.c
+++ minitalk/srcs_server/server.c
@@ -6,7 +6,7 @@
 /*   By: megardes <megardes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 13:05:24 by megardes          #+#    #+#             */
-/*   Updated: 2025/05/28 22:17:18 by megardes         ###   ########.fr       */
+/*   Updated: 2025/05/30 11:22:24 by megardes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ ssize_t	pow_2(ssize_t i)
 	return (2 * pow_2(i - 1));
 }
 
-t_uc	*recalloc_quad(t_uc *in, ssize_t pow)
+char	*recalloc_quad(char	*in, ssize_t pow)
 {
-	t_uc	*out;
+	char	*out;
 
 	if (!in)
-		return ((t_uc	*)ft_calloc(pow_2(pow), 1));
-	out = (t_uc	*)ft_calloc(pow_2(pow), 1);
+		return ((char *)ft_calloc(pow_2(pow), 1));
+	out = (char	*)ft_calloc(pow_2(pow), 1);
 	if (!out)
 		return (free(in), NULL);
 	ft_strlcpy(out, in, ft_strlen(in) + 1);
@@ -33,47 +33,45 @@ t_uc	*recalloc_quad(t_uc *in, ssize_t pow)
 	return (out);
 }
 
-int	get_bit(int signum, siginfo_t *new, void *old)
+void	get_bit(int signum, siginfo_t *new, void *old)
 {
-	static t_uc 	*out = NULL;
+	static char 	*out = NULL;
 	static ssize_t	bits = -1;
-	static ssize_t	calls = -1;
+	static ssize_t	calls = 0;
 	static ssize_t	pow = 1;
 
-	calls++;
 	bits++;
 	(void)old;
-	if (calls % 8 == 0 && calls / 8 + 2 >= pow_2(pow))
+	if (calls == 0 || calls + 2 >= pow_2(pow))
 		out = recalloc_quad(out, ++pow);
 	if (!out)
-		return (ft_putendl_fd("malloc failed!", 2), exit(1), 1);
-	out[calls / 8] += (signum / 12) * (1 << bits);
+		return (ft_putendl_fd("malloc failed!", 2), exit(1));
+	out[calls] += (signum == SIGUSR2) * (1 << bits);
 	if (bits == 7)
 	{
 		bits = -1;
-		if (!out[calls / 8])
+		if (!out[calls])
 			return (ft_putstr_fd(out, 1), free(out), out = NULL,
-					calls = -1, pow = 1, kill(new->si_pid, SIGUSR1));
+					calls = 0, pow = 1, (void)kill(new->si_pid, SIGUSR1));
 		else
 			kill(new->si_pid, SIGUSR2);
+		calls++;
 	}
 }
 
 int	main(void)
 {
-	int					pid;
 	struct sigaction	sig;
 
-	pid = getpid();
-	ft_putstr_fd("Server PID: ", 1);
-	ft_putnbr_fd(pid, 1);
-	ft_putendl_fd("", 1);
 	sig.sa_sigaction = &get_bit;
 	sig.sa_flags = SA_SIGINFO | SA_RESTART;
 	if (sigemptyset(&(sig.sa_mask)))
 		return (ft_putendl_fd("sigemptyset failed", 2), 1);
 	sigaction(SIGUSR1, &sig, NULL);
 	sigaction(SIGUSR2, &sig, NULL);
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putendl_fd("", 1);
 	while (1)
 		pause();
 	return (0);
