8997140f5a0493f923bf7f33dc0a5a4c5b08f6dd
Mehras Gardesh Meydani <megardes@c3r10p3.42vienna.com>
2025-07-26 13:43:56 +0200

    done

--- philo/philo/header/philo.h
+++ philo/philo/header/philo.h
@@ -6,7 +6,7 @@
 /*   By: megardes <megardes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 19:22:46 by megardes          #+#    #+#             */
-/*   Updated: 2025/07/08 21:26:50 by megardes         ###   ########.fr       */
+/*   Updated: 2025/07/26 13:37:56 by megardes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,20 @@
 # include <bits/pthreadtypes.h>
 # include <unistd.h>
 # include <string.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <stdio.h>
 
 typedef struct s_philo
 {
-	ssize_t			info_philos[5];
-	ssize_t			*philo_sits;
+	ssize_t			infos[5];
+	bool			alive;
+	ssize_t			*philo_rout;
 	pthread_t		*philos;
 	pthread_mutex_t	*mutex;
+	void			*(*route_0)(void *);
 	void			*(*route_1)(void *);
 	void			*(*route_2)(void *);
-	void			*(*route_3)(void *);
 }	t_philo;
 
 #endif
--- philo/philo/srcs/main.c
+++ philo/philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: megardes <megardes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 19:24:44 by megardes          #+#    #+#             */
-/*   Updated: 2025/07/08 21:29:40 by megardes         ###   ########.fr       */
+/*   Updated: 2025/07/26 13:40:23 by megardes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	ft_put_endl(char *str, int fd)
 ssize_t	atos(char *num)
 {
 	ssize_t	i;
-	ssize_t out;
+	ssize_t	out;
 
 	i = -1;
 	out = 0;
@@ -46,7 +46,7 @@ void	ft_putnbr(int i, int fd)
 
 int	ft_isdigit(char *in)
 {
-	ssize_t i;
+	ssize_t	i;
 
 	i = -1;
 	while (in[++i])
@@ -57,22 +57,94 @@ int	ft_isdigit(char *in)
 
 int	check_in(int argc, char **argv, t_philo *philo)
 {
+	philo->infos[4] = -1;
 	if (argc < 5)
-		return (ft_put_endl("Not enough arguments", 2), 0);	
+		return (ft_put_endl("Not enough arguments", 2), 0);
 	while (--argc > 0)
+	{
 		if ((argv[argc][0] != '+' || argv[argc]++) && !ft_isdigit(argv[argc]))
 			return (ft_put_str("Argument ", 2), ft_putnbr(argc, 2),
 				ft_put_endl(": is not a valid number", 2), 0);
 		else
-			philo->info_philos[argc - 1] = atos(argv[argc]);
+			philo->infos[argc - 1] = atos(argv[argc]);
+	}
 	return (1);
+}
+
+void	set_recur(ssize_t y, ssize_t *philo, ssize_t len, bool *in)
+{
+	ssize_t	x;
+
+	x = -1;
+	if (y == len)
+		return ((void)(*in = 1));
+	else
+	{
+		while (++x < 3)
+		{
+			
+			if (*in == 0 && (y == 0 || (y != len - 1 && philo[y - 1] != x)
+				|| (y == len - 1 && philo[y - 1] != x && philo[0] != x)))
+			{
+				if (!((y != len - 1 && philo[y - 1] != philo[y])
+				|| (y == len - 1 && philo[y - 1] != philo[y] && philo[0] != philo[y])))
+					philo[y] = x;
+				set_recur(y + 1, philo, len, in);
+			}
+		}	
+	}
+		
+}
+
+int	set_routine(t_philo *philo)
+{
+	ssize_t	i;
+
+	i = -1;
+	philo->philo_rout = (ssize_t *)malloc(philo->infos[0] * sizeof(ssize_t));
+	if (!philo->philo_rout)
+		return (0);
+	while (++i < philo->infos[0])
+		philo->philo_rout[i] = i % 3;
+	i = 0;
+	set_recur(0, philo->philo_rout, philo->infos[0], (bool *)(&i));
+	return (i);
+}
+
+
+
+int	create_philos(t_philo *philo)
+{
+	ssize_t	i;
+
+	philo->philos = (pthread_t *)malloc(philo->infos[0] * sizeof(pthread_t));
+	philo->mutex = (pthread_t *)malloc(philo->infos[0]
+			* sizeof(pthread_mutex_t));
+	if (!philo->philos || !philo->mutex)
+		return (free_all(philo), 0);
+	i = -1;
+	while (++i < philo->infos[0])
+		if (philo->philo_rout[i] % 3 == 0)
+			pthread_create(&philo->philo_rout[i], NULL, philo->route_0, philo);
+	i = -1;
+	while (++i < philo->infos[0])
+		if (philo->philo_rout[i] % 3 == 1)
+			pthread_create(&philo->philo_rout[i], NULL, philo->route_1, philo);
+	i = -1;
+	while (++i < philo->infos[0])
+		if (philo->philo_rout[i] % 3 == 2)
+			pthread_create(&philo->philo_rout[i], NULL, philo->route_2, philo);
 }
 
 int	main(int argc, char **argv)
 {
 	t_philo	philo;
 
-	memset(&philo.info_philos, -1, sizeof(philo.info_philos));
+	memset(&philo.infos, 0, sizeof(philo.infos));
 	if (!check_in(argc, argv, &philo))
-		return (0);
+		return (1);
+	if (!set_routine(&philo))
+		return (1);
+	if (!create_philos(&philo))
+		return (free_all(&philo), 1);
 }
